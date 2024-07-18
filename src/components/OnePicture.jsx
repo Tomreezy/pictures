9c2783cdb4a913f366e195e4e119a483080dd85c
Thomas <scelogumede95@gmail.com>
2024-07-18 14:14:45 +0200

    second commmit

--- src/components/OnePicture.jsx
+++ src/components/OnePicture.jsx
@@ -3,7 +3,7 @@ const OnePicture = ({name,img,occupation,age}) => {
   return (
     <article className='flex rounded-md gap-4 items-center border border-solid p-4 border-cyan-500 '>
         <div className='size-16 overflow-hidden  '>
-            <img src={img} className='w-full border-2 border-dashed border-indigo-400 h-full object-cover  rounded-full' />
+            <img src={img} className='w-full border-4 border-dashed border-indigo-400 h-full object-cover  rounded-full' />
         </div>
         <div>
         <p className='font-bold'>{name}</p>
